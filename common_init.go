6b264a490a8a804b07f0316a7076e6c34c929010
Matthew Butler <butler.matthew@gmail.com>
2017-08-03 11:45:54 -0300

    Move functions to files they belong in

--- common_init.go
+++ common_init.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"os"
+	"fmt"
+	"text/template"
+
+	"github.com/pkg/errors"
+)
+
 type FileItem struct {
 	Path   string
 	Tmpl   string
@@ -9,4 +17,54 @@ type FileItem struct {
 
 func (f *FileItem) Add(fi *FileItem) {
 	f.Childs = append(f.Childs, fi)
+}
+
+func walkFiles(f *FileItem, c initConf) error {
+	fmt.Println("Working on:", f.Path)
+	fmt.Printf("Children: %+v", f.Childs)
+	if !f.IsDir {
+		return mkfile(f.Path, f.Tmpl, c)
+	}
+
+	err := os.Mkdir(f.Path, 0755)
+	if err != nil {
+		return errors.Wrapf(err, "error creating directory %q", f.Path)
+	}
+
+	for _, ff := range f.Childs {
+		err := walkFiles(ff, c)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func mkfile(fn, tmpl string, conf initConf) (err error) {
+	file, err := os.Create(fn)
+	if err != nil {
+		err = errors.Wrapf(err, "unable to generate file: %s", fn)
+		return err
+	}
+	defer func() {
+		e := file.Close()
+		if e != nil {
+			err = errors.Wrapf(e, "error closing file %s", file.Name())
+		}
+		fmt.Println("closed file: ", file.Name())
+	}()
+
+	tmplate, err := template.New(fn).Parse(tmpl)
+	if err != nil {
+		err = errors.Wrapf(err, "error parsing template for file %q", fn)
+		return err
+	}
+	err = tmplate.Execute(file, conf)
+	if err != nil {
+		err = errors.Wrap(err, "error executing template")
+		return err
+	}
+
+	return nil
 }
--- dart_init.go
+++ dart_init.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	"text/template"
-
-	"github.com/pkg/errors"
-	"github.com/butlermatt/dslink-cmd/templates"
 	"path/filepath"
+
+	"github.com/butlermatt/dslink-cmd/templates"
 )
 
 func initDart(c initConf) error {
@@ -35,54 +32,4 @@ func initDart(c initConf) error {
 	lib.Add(src)
 
 	return walkFiles(root, c)
-}
-
-func walkFiles(f *FileItem, c initConf) error {
-	fmt.Println("Working on:", f.Path)
-	fmt.Printf("Children: %+v", f.Childs)
-	if !f.IsDir {
-		return mkfile(f.Path, f.Tmpl, c)
-	}
-
-	err := os.Mkdir(f.Path, 0755)
-	if err != nil {
-		return errors.Wrapf(err, "error creating directory %q", f.Path)
-	}
-
-	for _, ff := range f.Childs {
-		err := walkFiles(ff, c)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func mkfile(fn, tmpl string, conf initConf) (err error) {
-	file, err := os.Create(fn)
-	if err != nil {
-		err = errors.Wrapf(err, "unable to generate file: %s", fn)
-		return err
-	}
-	defer func() {
-		e := file.Close()
-		if e != nil {
-			err = errors.Wrapf(e, "error closing file %s", file.Name())
-		}
-		fmt.Println("closed file: ", file.Name())
-	}()
-
-	tmplate, err := template.New(fn).Parse(tmpl)
-	if err != nil {
-		err = errors.Wrapf(err, "error parsing template for file %q", fn)
-		return err
-	}
-	err = tmplate.Execute(file, conf)
-	if err != nil {
-		err = errors.Wrap(err, "error executing template")
-		return err
-	}
-
-	return nil
 }
