6f645ed37131afe4f1b0fbdb26304c2410b5c559
Matthew Butler <butler.matthew@gmail.com>
2017-08-08 10:21:04 -0300

    Add .gitignore template

--- dart_init.go
+++ dart_init.go
@@ -15,6 +15,7 @@ func initDart(c initConf) error {
 	root := &FileItem{Path: c.Dir, IsDir: true}
 	root.Add(&FileItem{Path: filepath.Join(c.Dir, "README.md"), Tmpl: templates.README})
 	root.Add(&FileItem{Path: filepath.Join(c.Dir, "LICENSE"), Tmpl: templates.LICENSE})
+	root.Add(&FileItem{Path: filepath.Join(c.Dir, ".gitignore"), Tmpl: templates.GitIgnore})
 	root.Add(&FileItem{Path: filepath.Join(c.Dir, "dslink.json"), Tmpl: templates.DSLinkJson})
 	root.Add(&FileItem{Path: filepath.Join(c.Dir, "pubspec.yaml"), Tmpl: templates.DartPubSpec})
 
